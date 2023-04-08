0e73796c0ef63841a248fad966c607731326e990
Aaryan Pashine <pashineaaryan@gmail.com>
2023-04-07 20:30:15 -0400

    what am i doing

--- sketch.js
+++ sketch.js
@@ -4,17 +4,19 @@ let w = WIDTH_INCHES * 96;
 let h = HEIGHT_INCHES * 96;
 let fSize = 9; // 8pts
 let font;
-let sponsorName = "McCulloch International Arts Council Ltd.";
-let nameArray = names.split(", ").sort();
+let temp = names.sort((a, b) =>
+  a.name.substring(0).localeCompare(b.name.substring(0))
+);
 
-// loop through nameArray
-for (let i = 0; i < nameArray.length; i++) {
-  if (nameArray[i].includes("$")) {
-    nameArray[i] = nameArray[i].replace(",000", "k");
-  }
-  if (nameArray[i].substring(nameArray[i].length - 1) != ")")
-    nameArray[i] += " (money)";
-}
+let nameArray = [...new Set(temp)];
+console.log(nameArray.length);
+let div = document.querySelector(".names");
+div.style.width = w + "px";
+div.style.height = h + "px";
+
+let currentDiv;
+let currentAlphabet = "A";
+
 function preload() {
   font = loadFont("./aktiv.otf");
 }
@@ -22,56 +24,39 @@ function setup() {
   createCanvas(w, h).parent("p5");
   textFont(font);
   frameRate(1);
+  currentDiv = document.querySelector(".names");
+  currentDiv.innerHTML += "<span class='alphabet'>A</span>";
 }
 
 function draw() {
   background(255);
 
   noFill();
-  strokeWeight(10);
+  strokeWeight(1);
   rect(0, 0, width, height);
 
   fill(0);
   textSize(fSize);
-  renderNames(nameArray);
+  // renderNames(nameArray);
+  for (const x of nameArray) addName(x.name, x.tier);
+  noLoop();
 }
 
 function mousePressed() {
   noLoop();
 }
 
-function renderNames(nameList) {
-  // draw name
-  let lastWidth = random(20);
-  let line = 0;
-  for (let i = 0; i < nameList.length; i++) {
-    if (nameList[i].includes("$")) textSize(fSize + 3);
-    else textSize(fSize);
-    let col = "red";
-    // check if next width if will cross margin: next line,
-    if (textWidth(nameList[i]) + 48 + lastWidth > width - 50) {
-      lastWidth = random(40);
-      line++;
-    }
-    if (random() > 0.5) col = "blue";
+function renderDivs(nameList) {
+  currentDiv.innerHtml += "Name";
+}
 
-    if (col === "blue") {
-      noFill();
-      stroke(50, 50, 255);
-      strokeWeight(0.1);
-    } else {
-      fill(255, 100, 100);
-    }
-    rect(
-      48 + lastWidth + random(-fSize / 2, fSize / 2),
-      48 + line * 14 + random(-fSize / 2, fSize / 2),
-      textWidth(nameList[i]) + random(-fSize / 2, fSize / 2),
-      fSize + random(-fSize / 2, fSize / 2)
-    );
-    fill(0);
-    noStroke();
-    text(nameList[i], 48 + lastWidth, 48 + fSize + line * 14);
-    // store its width
-    lastWidth += textWidth(nameList[i]) + random(10, fSize * 2.5);
-  }
+function addName(text, tier) {
+  if (text.toUpperCase().substring(0, 1) != currentAlphabet)
+    newDiv(text.toUpperCase().substring(0, 1));
+  currentDiv.innerHTML += `<span>${text}</span>`;
+}
+
+function newDiv(alphabet) {
+  currentAlphabet = alphabet;
+  currentDiv.innerHTML += `<span class="alphabet">${alphabet}</span>`;
 }
