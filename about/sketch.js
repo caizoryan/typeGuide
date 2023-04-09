78e61d77c6e4484bc28a4d2837fb09624a85ec6a
Aaryan Pashine <pashineaaryan@gmail.com>
2023-04-08 23:40:21 -0400

    fuck i need a smoke

--- about/sketch.js
+++ about/sketch.js
@@ -13,6 +13,9 @@ let para1 = d1.split(" ");
 let para2 = d2.split(" ");
 let para3 = d3.split(" ");
 
+let boxes = (d3 + d1 + d2).split(" ");
+console.log(boxes);
+
 function preload() {
   hairline = loadFont("./fonts/hairline.otf");
   maxi = loadFont("./fonts/maxiLight.otf");
@@ -31,18 +34,19 @@ function setup() {
   frameRate(1);
 
   let paraY = 48 * 6;
+  let leading = 14.4;
 
   tracking = 8;
   para1 = createLocations(para1, width - 48 - (center.x + 48));
-  para1 = addPositions(para1, center.x + 48, paraY, 15);
+  para1 = addPositions(para1, center.x + 48, paraY, leading);
 
-  tracking = 7;
+  tracking = 4;
   para2 = createLocations(para2, width - 48 - (center.x + 48));
   para2 = addPositions(
     para2,
     center.x + 48,
-    para1.length * 15 + paraY + paraBreak,
-    15
+    para1.length * leading + paraY + paraBreak,
+    leading
   );
 
   tracking = 7;
@@ -50,9 +54,12 @@ function setup() {
   para3 = addPositions(
     para3,
     center.x + 48,
-    para2.length * 15 + para1.length * 15 + paraY + paraBreak * 2,
-    15
+    para2.length * leading + para1.length * leading + paraY + paraBreak * 2,
+    leading
   );
+
+  boxes = createLocations(boxes, width);
+  boxes = addPositions(boxes, 100, -100, 12);
 }
 
 function draw() {
@@ -63,7 +70,10 @@ function draw() {
   rect(0, 0, width, height);
   line(center.x, 0, center.x, height);
   noStroke();
+  renderBoxes(boxes);
   fill(0);
+  textSize(fSize);
+  text("TYPE3: 2023", center.x + 48, 48 + fSize * 1.2);
   textFont(maxi);
   textSize(fSize * 3);
   text("HyperbolicCod3x", center.x + 48 + 6, 48 + fSize * 3);
@@ -80,17 +90,22 @@ function renderBoxes(words) {
     for (let x = 0; x < words[y].words.length; x++) {
       let word = words[y].words[x].word;
       let pos = words[y].words[x].position;
-      fill(0);
       textSize(fSize);
-      if (word.length < 3) textFont(bold);
-      else if (word.length < 6) textFont(medium);
-      else if (word.length < 10) textFont(regular);
-      else textFont(thin);
+      if (word.length < 3) fill(240, 20);
+      else if (word.length < 6) fill(230, 20);
+      else if (word.length < 10) fill(220, 20);
+      else fill(240);
 
+      fill(255, 255, 0);
       let tw = textWidth(word);
-      text(word, pos.x, pos.y);
-      fill(255, 0, 255);
-      circle(pos.x + tw + word.length / 4, pos.y - fSize / 4, 2);
+      // circle(pos.x, pos.y + x * tw, 80 / tw);
+      rectMode(CENTER);
+      push();
+      translate(pos.x, pos.y + x * tw);
+      rotate(map(pos.y + x * tw, 0, 100, 1, 0));
+      rect(0, 0, 3, 800 / tw);
+      pop();
+      fill(0);
       fill(255, 0);
     }
   }
@@ -173,5 +188,5 @@ function addPositions(lines, xStart, yStart, leading) {
 }
 
 function mousePressed() {
-  // saveCanvas("frame", "jpg");
+  saveCanvas("frame", "jpg");
 }
